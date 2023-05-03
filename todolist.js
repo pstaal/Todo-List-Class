3735a35ec41df3025875392dcf825498fd97afea
peter staal <peterstaal@MBP-van-peter.home>
2023-05-03 16:34:21 +0200

    finished todlist class methods

--- todolist.js
+++ todolist.js
@@ -27,10 +27,6 @@ class Todo {
   isDone() {
     return this.done;
   }
-
-  getTitle() {
-    return this.title;
-  }
 }
 
 // This class represents a collection of Todo objects.
@@ -43,8 +39,65 @@ class TodoList {
     this.todos = [];
   }
 
-  // rest of class needs implementation
-}
+  add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError("can only add Todo objects");
+    }
 
-let list = new TodoList("Today's Todos");
-console.log(list); // TodoList { title: "Today's Todos", todos: [] }
+    this.todos.push(todo);
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1);
+  }
+
+  toString() {
+    let title = `---- ${this.title} ----`;
+    let list = this.todos.map(todo => todo.toString()).join("\n");
+    return `${title}\n${list}`;
+  }
+
+  _validateIndex(index) { // _ in name indicates "private" method
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+}
